be680e135bf1c26a8a78a82bca1d0483c191256b
mrhakimov7 <hakimovabdurazzoq2008agmail.com>
2023-08-15 15:01:00 +0500

    aa

--- src/components/Sefeatures/Sefeatures.jsx
+++ src/components/Sefeatures/Sefeatures.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Sefeatures.scss';
 
 function Sefeatures() {
   return (
     <div className='Sefeatures'>
-        <h1>Sefeatures</h1>
+      <div className="container">
+        <div className="all"> 
+         <div className="title">
+          <h2>Explore Course</h2>
+          <p>Ut sed eros finibus, placerat orci id, dapibus.</p>
+         </div>
+         <div className="box1">
+            <div className="box1_title">
+            <i class="bi bi-palette-fill"></i>
+            <p>Lorem, ipsum.</p>
+            </div>
+            <div className="menu">
+                {/* sss */}
+            </div>
+         </div>
+         <div className="box2">
+          
+         </div>
+        </div>
+      </div>
     </div>
   )
 }
