439a938e8c74bce45943d16997f4a85dfd01b9b6
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-08-18 18:04:49 +0500

    fixed code files

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import './Course.scss'
+import img from '../../img/Detail.png'
 function Courses() {
   return (
     <div className='Courses'>
-        <h1>Courses</h1>
+        <div className="alls"> 
+         <div className="title">
+          <h2>Explore Course</h2>
+          <p>Ut sed eros finibus, placerat orci id, dapibus.</p>
+         </div>
+         <div className="box1">
+            <div className="box_title">
+            <i class="bi bi-palette-fill"></i>
+            <p>Lorem, ipsum.</p>
+            </div>
+            <div className="menu">
+               
+            </div>
+         </div>
+         <div className="box2">
+         <div className="box_title">
+         <i class="bi bi-globe-americas"></i>
+            <p>Quisque a Consequat</p>
+            </div>
+            <div className="menu">
+               
+            </div>
+         </div>
+         <div className="box3">
+         <div className="box_title">
+         <i class="bi bi-clipboard"></i>
+            <p>Quisque a Consequat</p>
+            </div>
+            <div className="menu">
+               
+            </div>
+         </div>
+        </div>
     </div>
   )
 }
