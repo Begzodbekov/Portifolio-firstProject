8f4c34ce78935a241d449504b2ae7b4611b016cb
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-08-27 21:32:32 +0500

    header and hero done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import './Header.scss'
+import Logo from '../../img/header-logo.png'
 function Header() {
   return (
     <div className='Header'>
-        <h1>Header</h1>
+      <div className="nav">
+        <div className="container">
+          <div className="nav_inner">
+             <img src={Logo} alt="" />
+             <ul className='nav_list'>
+                <li>Home</li>
+                <li>Courses</li>
+                <li>Careers</li>
+                <li>Blog</li>
+                <li>About Us</li>
+                <li><button className='nav_login'>Login</button></li>
+                <li><button className='nav_sign'>Sign Up</button></li>
+             </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
-
+import './Hero.scss'
+import Calendar from '../../img/hero-calendar.png'
+import Class from '../../img/hero-men-class.png'
+import Message from '../../img/hero-pochta.png'
+import Voice from '../../img/hero-voice.png'
+import Watch from '../../img/hero-watch.png'
+import Woman from '../../img/herowoman-background.png'
 function Hero() {
   return (
     <div className='Hero'>
-        <h1>Hero</h1>
+        <div className="container">
+          <div className="hero-innner">
+           
+              <div className='hero-left-inner'>
+                   <h1><span>Studying</span> Online is now much easier</h1>
+                   <p>TOTC is an interesting platform that will teach you in more an interactive way</p>
+                <div className='hero-buttons'>
+                  <button className='hero-join'>Join for free</button>
+                  <a href="#"><img src={Watch} alt="" /></a>
+                  <h3>Watch how it works</h3>
+                </div>
+              </div>
+            
+            
+              <div className='hero-right-inner'>
+                <div className='hero-right-items1'>
+                  <div className="hero-assisted">
+                    <img src={Calendar} alt="" />
+                    <div>
+                      <h3>250k</h3>
+                      <p>Assisted Student</p>
+                    </div>
+                  </div>
+                  <div className="hero-class">
+                    <div className='hero-div'>
+                      <img src={Class} alt="" />
+                      <div>
+                        <h2>User Experience Class</h2>
+                        <p>Today at 12.00 PM</p>
+                      </div>
+                    </div>
+                        <button className='hero-class-join'>Join Now</button>
+                  </div>
+                </div>
+                <div className="hero-right-item2">
+                  <img className='hero-voice' src={Voice} alt="" />
+                  <div className="hero-congratulations">
+                    <img src={Message} alt="" />
+                    <div>
+                      <h2>Congratulations</h2>
+                      <p>Your admission completed</p>
+                    </div>
+                  </div>
+                </div>
+              {/* <div>  <img className='hero-right-background' src={Woman} alt="" /></div> */}
+              </div>
+            
+          </div>
+        </div>
     </div>
   )
 }
