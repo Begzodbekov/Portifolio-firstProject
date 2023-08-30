023e99450fc8da89cfdc1f1df07f25658d03a1b6
ortiqboy2005 <ortiqboy0804>
2023-08-30 17:48:12 +0500

    sefeatures completed

--- src/components/Sefeatures/Sefeatures.jsx
+++ src/components/Sefeatures/Sefeatures.jsx
@@ -17,7 +17,7 @@ function Sefeatures() {
             <h1 className="list__2"> Quizzes,</h1>
             <h1 className="list__1">Tests</h1>
             <p>
-              
+
               Easily launch live assignments, quizzes, and tests. Student
               results are automatically entered in the online gradebook.
             </p>
@@ -28,29 +28,29 @@ function Sefeatures() {
             <h1 className="text">Class Management </h1>
             <h1 className="text2">Tools for Educators</h1>
             <p>
-              Class provides tools to help run and manage the class such as
-              Class Roster, Attendance, and more. With the Gradebook, teachers
-              can review and grade tests and quizzes in real-time.
-            </p>
+              Class provides tools to help run and manage the class such as Class Roster,
+              Attendance, and more. With the Gradebook, teachers can review and grade tests and
+              quizzes in real-time.</p>
           </div>
           <div className="Sefeatures__img2">
             <img src={sefeatures2} alt="" />
           </div>
         </div>
-      </div>
-      <div className="sefeatures-part3">
-        <div className="sefeatures__img3">
-          <img src={sefeatures3} alt="" />
+
+        <div className="sefeatures-part3">
+          <div className="sefeatures__img3">
+            <img src={sefeatures3} alt="" />
+          </div>
+          <div className="sefeatures__list3">
+            <h1 className="list1">One-on-One</h1>
+            <h1 className="list2"> Discussions</h1>
+            <p className="list3">Teachers and teacher assistants can talk with students privately without leaving the Zoom environment.</p>
+          </div>
         </div>
-        <div className="sefeatures__list3">
-          <h1 className="list1">One-on-One</h1>
-          <h1 className="list2"> Discussions</h1>
-          <p className="list3">Teachers and teacher assistants can talk with students privately without leaving the Zoom environment.</p>
-        </div>
+        <button className="button">See more features</button>
       </div>
-      <button className="button">See more features</button>
     </div>
-    
+
   );
 
 }
