1d4b02adadfc00cb79d131821bd0861507ca9a43
ortiqboy2005 <ortiqboy0804@gmail.com>
2023-09-04 17:43:05 +0500

    Dcontent

--- src/components/Dcontent/Dcontent.jsx
+++ src/components/Dcontent/Dcontent.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import './Dcontent.scss'
 
 
 function Dcontent() {
   return (
-    <div>
-     Dcontent
+    <div className='Dcontent'>
+      <div className="container">
+        <div className="dcontent__inner">
+          <div className="dcontent-right">
+          <ul className='dcontent__list'>
+            <li className='dcontent__list-item'>
+              Overview
+            </li>
+            <li className='dcontent__list-item'>
+              Overview
+            </li>
+            <li className='dcontent__list-item'>
+              Overview
+            </li>
+            <li className='dcontent__last-item'>
+              Overview
+            </li>
+          </ul>
+          </div>
+
+        </div>
+      </div>
+     
     </div>
   )
 }
