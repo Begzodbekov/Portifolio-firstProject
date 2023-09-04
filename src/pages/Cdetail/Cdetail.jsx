842dbb9fe6c03fe21688e0203c9d18c2f9ef30ee
ortiqboy2005 <ortiqboy0804@gmail.com>
2023-09-04 16:54:43 +0500

    iegfsadh

---
+++ src/pages/Cdetail/Cdetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dcontent from '../../components/Dcontent/Dcontent'
+import Dhero from '../../components/Dhero/Dhero'
+
+function Cdetail() {
+  return (
+    <div>
+      <Dhero/>
+      <Dcontent/>
+    </div>
+  )
+}
+
+export default Cdetail
