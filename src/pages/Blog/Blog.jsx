eb59196643d20ec2f6252133c27f6f8be9fc9222
AlimovSarvar <alimovsarvar.12.08.2008@gmail.com>
2023-09-04 17:59:42 +0500

    marketing tugadi

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Blog.scss'
 import BlogUI from '../../components/BlogUI/BlogUI'
 import Related from '../../components/Related/Related'
-import BlogUI from '../../components/BlogUI/BlogUI'
+import Marketing from '../../components/Marketing/Marketing'
 function Blog() {
   return (
     <div className='Blog'>
