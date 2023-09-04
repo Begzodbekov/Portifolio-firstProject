25085281b0c587fec92917c678b9756b6d0ec18d
mrhakimov7 <hakimovabdurazzoq2008@gmail.com>
2023-09-04 18:13:42 +0500

    education chala

--- src/pages/Cdetail/Cdetail.jsx
+++ src/pages/Cdetail/Cdetail.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Dcontent from '../../components/Dcontent/Dcontent'
 import Dhero from '../../components/Dhero/Dhero'
+import Dtotc from '../../components/Dtotc/Dtotc'
+import Education from '../../components/Education/Education'
 
 function Cdetail() {
   return (
     <div>
       <Dhero/>
       <Dcontent/>
+      <Dtotc/>
+      <Education/>
     </div>
   )
 }
